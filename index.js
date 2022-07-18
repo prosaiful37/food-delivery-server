1788e77911adc54135e8babaeb7c609725d8f48b
Saiful Islam <57891501+prosaiful37@users.noreply.github.com>
2022-07-18 17:33:14 +0600

    user api create

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -20,6 +21,8 @@ async function run() {
   try {
     await client.connect();
     const productsCollection = client.db('pizza_app').collection('products');
+    const ordersCollection = client.db('pizza_app').collection('orders');
+    const usersCollection = client.db('pizza_app').collection('users');
 
 
 
@@ -40,7 +43,42 @@ async function run() {
       res.send(order)
     })
 
+    // user update
+    app.put('/users/:email', async(req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = {email : email};
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await usersCollection.updateOne(filter, options, updateDoc)
+      res.send(result) 
+    })
 
+    // single data post in order
+    app.post('/orders', async(req, res) => {
+      const order = req.body;
+      const result = await ordersCollection.insertOne(order)
+      res.send(result);
+    })
+
+    // get all orders
+    app.get('/orders', async(req, res) => {
+      const query = {};
+      const cursor = ordersCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
+  })
+
+
+    // order data show table
+    app.get('/orders', async(req, res) => {
+      const email = req.query.email;
+      const query = { email: email }
+      const order = await ordersCollection.find(query).toArray();
+      res.send(order);
+    })
 
 
 
