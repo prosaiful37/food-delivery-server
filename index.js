2ade5959b5523d374d9da1a5abf45915913e35d7
Saiful Islam <57891501+prosaiful37@users.noreply.github.com>
2022-07-19 18:17:59 +0600

    jwt token access

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -20,70 +20,68 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const productsCollection = client.db('pizza_app').collection('products');
-    const ordersCollection = client.db('pizza_app').collection('orders');
-    const usersCollection = client.db('pizza_app').collection('users');
-
-
-
+    const productsCollection = client.db("pizza_app").collection("products");
+    const ordersCollection = client.db("pizza_app").collection("orders");
+    const usersCollection = client.db("pizza_app").collection("users");
 
     // get all products
-    app.get('/products', async(req, res) => {
-        const query = {};
-        const cursor = productsCollection.find(query);
-        const products = await cursor.toArray();
-        res.send(products);
-    })
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
 
     // single menu product show
-    app.get('/products/:id', async(req, res) => {
+    app.get("/products/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: ObjectId(id) }
+      const query = { _id: ObjectId(id) };
       const order = await productsCollection.findOne(query);
-      res.send(order)
-    })
+      res.send(order);
+    });
 
-    // user update
-    app.put('/users/:email', async(req, res) => {
+    // user update by email
+    app.put("/users/:email", async (req, res) => {
       const email = req.params.email;
       const user = req.body;
-      const filter = {email : email};
+      const filter = { email: email };
       const options = { upsert: true };
       const updateDoc = {
         $set: user,
       };
-      const result = await usersCollection.updateOne(filter, options, updateDoc)
-      res.send(result) 
-    })
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET,  { expiresIn: '1h' })
+      res.send({result, token});
+    });
+
 
     // single data post in order
-    app.post('/orders', async(req, res) => {
+    app.post("/orders", async (req, res) => {
       const order = req.body;
-      const result = await ordersCollection.insertOne(order)
+      const result = await ordersCollection.insertOne(order);
       res.send(result);
-    })
+    });
 
     // get all orders
-    app.get('/orders', async(req, res) => {
-      const query = {};
-      const cursor = ordersCollection.find(query);
-      const orders = await cursor.toArray();
-      res.send(orders);
-  })
-
+    // app.get("/orders", async (req, res) => {
+    //   const query = {};
+    //   const cursor = ordersCollection.find(query);
+    //   const orders = await cursor.toArray();
+    //   res.send(orders);
+    // });
 
     // order data show table
-    app.get('/orders', async(req, res) => {
+    app.get("/orders", async (req, res) => {
       const email = req.query.email;
-      const query = { email: email }
+      const query = { email: email };
       const order = await ordersCollection.find(query).toArray();
       res.send(order);
-    })
-
-
-
+    });
   } finally {
-   
   }
 }
 
