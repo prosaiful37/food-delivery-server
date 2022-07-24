698d66cec8112aa2ffc0a9dfe0fc85f0978990c6
Saiful Islam <57891501+prosaiful37@users.noreply.github.com>
2022-07-24 22:17:29 +0600

    single orders data api update

--- index.js
+++ index.js
@@ -18,21 +18,20 @@ const client = new MongoClient(uri, {
 });
 
   // verify jwt
-  function verifyJWT(req, res, next){
+  function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
-    if(!authHeader){
-      return res.status(401).send({message: 'Unauthorized access'})
+    if (!authHeader) {
+      return res.status(401).send({ message: 'UnAuthorized access' });
     }
     const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET , function(err, decoded) {
-      if(err){
-        return res.status(403).send({message : 'Forbidden Access'})
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+      if (err) {
+        return res.status(403).send({ message: 'Forbidden access' })
       }
       req.decoded = decoded;
-     next();
+      next();
     });
-
-    }
+  }
 
 
 async function run() {
@@ -79,19 +78,35 @@ async function run() {
     });
 
 
-    // user admin by email
-    app.put("/users/admin/:email", verifyJWT() async (req, res) => {
+    app.get('/admin/:email', async(req, res) => {
       const email = req.params.email;
-      const filter = { email: email };
-      const updateDoc = {
-        $set: {role: 'admin'},
-      };
-      const result = await usersCollection.updateOne(
-        filter,
-        updateDoc
-      );
-      const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET,  { expiresIn: '1h' })
-      res.send({result, token});
+      const user = await usersCollection.findOne({email: email})
+      const isAdmin = user.role === "admin";
+      res.send({admin: isAdmin});
+    })
+
+
+    // user admin by email
+    app.put("/users/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const requister = req.decoded.email;
+      const requisterAccount = await usersCollection.findOne({email: requister});
+      if(requisterAccount.role === 'admin'){
+        const filter = { email: email };
+        const updateDoc = {
+          $set: {role: 'admin'},
+        };
+        const result = await usersCollection.updateOne(
+          filter,
+          updateDoc
+        );
+        const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET,  { expiresIn: '1h' })
+        res.send(result);
+      }
+      else{
+        res.status(403).send({message: 'forbidden access'})
+      }
+      
     });
 
 
@@ -103,27 +118,26 @@ async function run() {
     });
 
     // get all orders
-    app.get("/orders", verifyJWT, async (req, res) => {
-      const query = {};
-      const cursor = ordersCollection.find(query);
-      const orders = await cursor.toArray();
-      res.send(orders);
-    });
+    // app.get("/orders", verifyJWT, async (req, res) => {
+    //   const query = {};
+    //   const cursor = ordersCollection.find(query);
+    //   const orders = await cursor.toArray();
+    //   res.send(orders);
+    // });
 
 
-    // order data show table in email
-    app.get("/orders", verifyJWT, async (req, res) => {
-      const email = req.query.email;
-      const decodedEmail = req.decodedEmail.email;
-      if(email === decodedEmail){
-        const query = { email: email };
+
+      app.get('/orders', verifyJWT, async (req, res) => {
+      const userEmail = req.query.userEmail;
+      const decodedEmail = req.decoded.email;
+      if (userEmail === decodedEmail) {
+        const query = { userEmail: userEmail };
         const order = await ordersCollection.find(query).toArray();
         return res.send(order);
       }
-      else{
-        return res.status(403).send({message: 'Forbidden access'});
+      else {
+        return res.status(403).send({ message: 'forbidden access' });
       }
-      
     });
 
     // all user api 
